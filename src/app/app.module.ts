235fbe1145eccafae88ebc5e1ddcc8ea1706c0b6
unknown <test2mail2698@gmail.com>
2024-07-04 06:16:45 +0530

    added content and features to Customerhelp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AccountComponent } from './account/account.component';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { CustomerhelpComponent } from './customerhelp/customerhelp.component';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -45,11 +46,11 @@ const approute: Routes=[
     ContactComponent,
     AccountComponent,
     SearchComponent,
- 
+    CustomerhelpComponent,
     
   ],
   imports: [
-    BrowserModule,FormsModule,
+    BrowserModule,FormsModule,CommonModule,      
     AppRoutingModule,RouterModule.forRoot(approute),
   ],
   providers: [],
--- src/app/customerhelp/customerhelp.component.ts
+++ src/app/customerhelp/customerhelp.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './customerhelp.component.css'
 })
 export class CustomerhelpComponent {
+ show:number=1;
+ show1:number=1;
+ show2:number=1;
 
+ showparagraph(){
+  if( this.show === 1){ this.show=0; }
+  else{  this.show=1; }
+ 
+ }
+ showparagraph1(){
+  if( this.show1=== 1){ this.show1=0; }
+  else{  this.show1=1; }
+ }
+ showparagraph2(){
+  if( this.show2=== 1){ this.show2=0; }
+  else{  this.show2=1; }
+ }
 }
